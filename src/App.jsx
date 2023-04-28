6d8f4896338786e5369c09b88d5b73d816f2c38a
jarrydharris <mail@jarrydharris.org>
2023-04-28 16:39:10 +1000

    fix: no image displayed in Featured

--- src/App.jsx
+++ src/App.jsx
@@ -56,6 +56,7 @@ export default App
 const boxStyle = {
     borderRadius: 2,
     padding: "0.5em",
+    backgroundColor: "#fff",
 }
 
 const containerStyle = {
--- src/Featured.jsx
+++ src/Featured.jsx
@@ -1,31 +1,45 @@
-import Stack from "@mui/material/Stack";
 import {Container} from "@mui/material";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+///*Photo by <a href="https://unsplash.com/@joshrh19?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Joshua Rawson-Harris</a> on <a href="https://unsplash.com/photos/YNaSz-E7Qss?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>*/
 
 function Featured() {
     return (
         <>
-        <Container>
-            <Stack direction={"row"} spacing={2}>
+            <Container style={containerStyle}>
                 <Box style={boxStyle}>
-                    <Typography variant={"h6"}>Latest Special</Typography>
+                    <img width="100%" src="./src/assets/joshua-rawson-harris-YNaSz-E7Qss-unsplash.jpg" alt={"Joshua Rawson-Harris"}/>
+
+                    <Typography
+                        position='absolute'
+                        color="#fff"
+                        variant={"h4"}
+                    >
+
+                    </Typography>
                 </Box>
-            </Stack>
-        </Container>
+            </Container>
         </>
     )
 }
 
 const boxStyle = {
     border: 1,
-    borderRadius: 2,
-    height: '20vh',
+    borderRadius: 4,
+    height: "100%",
     width: "100%",
-    backgroundColor: '#ffffff',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    overflow: 'hidden',
+}
+
+const containerStyle = {
+    padding: 0,
+    display: 'flex',
+    justifyItems: 'stretch',
+    alignItems: 'flex-grow',
+    height: '15vh',
 }
 
 export default Featured
