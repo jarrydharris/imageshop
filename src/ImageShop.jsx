8a411a4b5d7608adb10a7f07e43b12ba27faa723
jarrydharris <mail@jarrydharris.org>
2023-05-11 21:16:30 +1000

    feat: Take a photo to query for item that exists in database

--- src/ImageShop.jsx
+++ src/ImageShop.jsx
@@ -1,48 +1,110 @@
 import {Button, Modal} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import weaviate from "weaviate-client";
+import {useState} from "react";
 
-function handleInputChange(event) {
-    const newValue = event.target.files;
-    const image = document.querySelector('#showImage');
-    image.src = URL.createObjectURL(newValue[0]);
-    const reader = new FileReader();
-    reader.readAsDataURL(newValue[0]);
-    console.log("onload about to be called");
-    reader.onload = () => {
-        console.log("onload called");
-        const base64String = reader.result;
-        console.log(base64String);
-    };
-}
+const client = weaviate.client({
+    scheme: 'https',
+    host: 'phoneapp.jarrydharris.org',
+    headers: ["Content-Type", "application/json"],
+});
+
 
 function ImageShop(props) {
+    const [searchResult, setSearchResult] = useState(null);
+
+    function handleInputChange(e) {
+        const newValue = e.target.files;
+        console.log(newValue);
+        const image = document.querySelector('#showImage');
+        image.src = URL.createObjectURL(newValue[0]);
+        const reader = new FileReader();
+        reader.readAsDataURL(newValue[0]);
+        let result = null;
+        reader.onload = () => {
+            const base64String = reader.result.substring(23);
+            client.graphql
+                .get()
+                .withClassName('Images')
+                .withFields(['labelName', 'image'])
+                .withNearImage({image: base64String})
+                .withLimit(3)
+                .do()
+                .then((res) => {
+                    result = res.data.Get.Images[0].image;
+
+                    setSearchResult("data:image/jpeg;base64," + result);
+                    image.src = searchResult;
+                })
+                .catch(err => {
+                    console.error(err)
+                });
+        };
+    }
+
+    const clearImageAndClose = () => {
+        const image = document.querySelector('#showImage');
+        image.src = "";
+        setSearchResult(null);
+        props.onClick();
+    }
+
+    const clearImage = () => {
+        const image = document.querySelector('#showImage');
+        image.src = "";
+        setSearchResult(null);
+    }
+
+    const addToCart = () => {
+    }
+
 
     return (
-        <>
-            <Modal open={props.open}>
-                <Box sx={outerContainerStyle}>
-
-                    <Typography variant="h5">
-                        Welcome to ImageShop!
-                    </Typography>
-                    <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                        <img id="showImage" height="300px" width="300px"></img>
-                        <Typography>Take a photo or select an image from your gallery</Typography>
-                        <input
-                            id="imageInput"
-                            type="file"
-                            name="picture"
-                            accept="image/*"
-                            capture="environment"
-                            onChange={handleInputChange}
-                        >
-                        </input>
-                    </Box>
-                    <Button variant="contained" sx={{}} onClick={props.onClick}>Close</Button>
+        <Modal open={props.open} onClose={clearImageAndClose}>
+            <Box sx={outerContainerStyle}>
+                <Typography variant="h5">
+                    Welcome to ImageShop!
+                </Typography>
+                <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                    {(searchResult === null) ?
+                        <>
+                            <img id="showImage" height="200px" width="200px"></img>
+                            <Typography>Take a photo or select an image from your gallery</Typography>
+                            <input
+                                id="imageInput"
+                                type="file"
+                                name="picture"
+                                accept="image/*"
+                                capture="environment"
+                                onChange={handleInputChange}
+                            />
+                        </> :
+                        <>
+                            <Typography>This is what we found!</Typography>
+                            <Box sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                            }}>
+                                <img src={searchResult} id="showImage" height="300px" width="300px"></img>
+                                <Typography>Name</Typography>
+                                <Typography>$0.00</Typography>
+                                <Typography>Description</Typography>
+                            </Box>
+                            <Box sx={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                gap: '0.5em'
+                            }}>
+                                <Button variant="contained" sx={{}} onClick={addToCart}>Add to cart</Button>
+                                <Button variant="contained" sx={{}} onClick={clearImage}>New search</Button>
+                            </Box>
+                        </>
+                    }
                 </Box>
-            </Modal>
-        </>
+                <Button variant="contained" sx={{}} onClick={clearImageAndClose}>Close</Button>
+            </Box>
+        </Modal>
     )
 }
 
@@ -53,8 +115,9 @@ const outerContainerStyle = {
     alignContent: 'center',
     alignItems: 'center',
     justifyContent: 'space-around',
-    height: "90vh",
-    maxHeight: "800px",
+    maxHeight: "831px",
+    minHeight: "540px",
+    height:"93vh",
     minWidth: "340px",
     width: "100vw",
     maxWidth: "444px",
